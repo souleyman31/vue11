1fb7737e563a9bdabddc6f47727e8cbb2decb95d
Souleyman <soulmandra@gmail.com>
2021-09-28 14:27:28 +0200

    backend

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2021</p>
+    <!-- <a href="/#about"> About</a> -->
+    <router-link to="/about"> About</router-link>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+<style scoped>
+a {
+  color: #656565;
+}
+footer {
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ text }}</h1>
+    <Button
+      @toggle-add-task="$emit('toggle-add-task')"
+      :color="showAddTask ? 'red' : 'green'"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+    />
+  </header>
+</template>
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  components: { Button },
+  props: {
+    text: String,
+    showAddTask: Boolean
+  }
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  color: blue;
+}
+</style>
